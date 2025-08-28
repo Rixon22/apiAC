4810a28f321de957d3b5c838fee57fce0ba67eee
Rogelio Seañez <roys0421@gmail.com>
2025-08-28 07:28:56 -0600

    [MOD] nuggets

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae4e3f08fcc6df739508280224f4691c70bdabcf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d115e148f996c2cdaf0c947f98d93b5395bc4d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
