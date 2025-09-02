c894d3a280babd57796a37fc8a3aff4925d77dab
Rogelio Seañez <roys0421@gmail.com>
2025-09-02 07:29:03 -0600

    [MOD] async

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4810a28f321de957d3b5c838fee57fce0ba67eee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b984ab0f6e86989d0ca3560eb52e9862f2b3d25f")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
