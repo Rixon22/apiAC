e2d81ca9f334770a70032d8d50fd7fed4b535839
Rogelio Seañez <roys0421@gmail.com>
2025-09-03 07:38:12 -0600

    [MOD] client

--- api/API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ api/API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c894d3a280babd57796a37fc8a3aff4925d77dab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4837f14eff912f205849b62fcc383c216b387b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
