2c2a34d54106a21243712c44071c9627839a9c51
Rogelio Seañez <roys0421@gmail.com>
2025-09-11 07:24:34 -0600

    [MOD] emial validation

---
+++ api/API/Controllers/AccountController.cs
@@ -0,0 +1,39 @@
+using System.Security.Cryptography;
+using API.Data;
+using API.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    // https://localhost:5001/api/account
+    public class AccountController(AppDbContext context) : BaseApiController
+    {
+        [HttpPost("register")]
+        public async Task<ActionResult> Register(RegisterRequest request)
+        {
+            if (await EmailExists(request.Email))
+                return BadRequest("Email is already in use");
+
+            using var hmac = new HMACSHA512();
+
+            var user = new Entities.AppUser
+            {
+                Email = request.Email.ToLower(),
+                DisplayName = request.DisplayName,
+                PasswordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(request.Password)),
+                PasswordSalt = hmac.Key
+            };
+
+            context.Users.Add(user);
+            await context.SaveChangesAsync();
+            return Ok(user);
+        }
+
+        private async Task<bool> EmailExists(string email)
+        {
+            return await context.Users.AnyAsync(x => x.Email == email.ToLower());
+        }
+    }
+}
--- api/API/Controllers/MembersController.cs
+++ api/API/Controllers/MembersController.cs
@@ -4,25 +4,23 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-namespace API.Controllers
-{
-    [Route("api/[controller]")] // https://localhost:5001/api/members
-    [ApiController]
-    public class MembersController(AppDbContext context) : Controller
-    {
-        [HttpGet]
-        public async Task<ActionResult<IReadOnlyList<AppUser>>> GetMembers()
-        {
-            var members = await context.Users.ToListAsync();
-            return members;
-        }
+namespace API.Controllers;
 
-        [HttpGet("{id}")] // https://localhost:5001/api/members/{id}
-        public async Task<ActionResult<AppUser>> GetMember(string id)
-        {
-            var member = await context.Users.FindAsync(id);
-            if (member == null) return NotFound();
-            return member;
-        }
+// https://localhost:5001/api/members
+public class MembersController(AppDbContext context) : BaseApiController
+{
+    [HttpGet]
+    public async Task<ActionResult<IReadOnlyList<AppUser>>> GetMembers()
+    {
+        var members = await context.Users.ToListAsync();
+        return members;
+    }
+
+    [HttpGet("{id}")] // https://localhost:5001/api/members/{id}
+    public async Task<ActionResult<AppUser>> GetMember(string id)
+    {
+        var member = await context.Users.FindAsync(id);
+        if (member == null) return NotFound();
+        return member;
     }
 }
