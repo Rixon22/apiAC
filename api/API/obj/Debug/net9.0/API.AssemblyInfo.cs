223e79b47f48b8f5f56df81de902ba60a8001279
Rogelio Seañez <roys0421@gmail.com>
2025-09-19 22:27:41 -0600

    [MOD] agregando estilo a navbar

--- api/API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ api/API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68deb9e4f9368b158ce9b65fddb5b3a52d95db18")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d840f1f972daca8b0b5e9d96c89e82b75fe456a0")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
