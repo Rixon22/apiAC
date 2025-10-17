bd3a8a4c722ddaab77b1cb4f28fe6dbbee9aa66e
Rogelio Seañez <roys0421@gmail.com>
2025-10-17 10:45:09 -0600

    [MOD] repository

--- api/API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ api/API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff1583046cf13b342167fadef41acea10e93eb61")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9431cde07f2903666277bbaa2ae3efd9010c3c79")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
