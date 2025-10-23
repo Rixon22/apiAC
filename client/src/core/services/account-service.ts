97c44f72dfffb0e130f7ffa5009c04f15101d17e
Rogelio Seañez <roys0421@gmail.com>
2025-10-23 07:42:58 -0600

    [MOD] member list

--- client/src/core/services/account-service.ts
+++ client/src/core/services/account-service.ts
@@ -3,6 +3,7 @@ import { inject, Injectable, signal } from '@angular/core';
 import { tap } from 'rxjs/operators';
 import { LoginCreds, RegisterCreds, User } from '../../types/user';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +12,7 @@ export class AccountService {
   private http = inject(HttpClient);
   currentUser = signal<User | null>(null);
 
-  baseUrl = 'https://localhost:7177/api/';
+  baseUrl = environment.apiUrl;
 
   register(creds: RegisterCreds): Observable<User> {
     return this.http.post<User>(this.baseUrl + "account/register", creds).pipe(
--- client/src/features/members/member-list/member-list.ts
+++ client/src/features/members/member-list/member-list.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject, Inject } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Member } from '../../../types/members';
+import { MembersService } from '../../../core/services/memebers-service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-member-list',
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './member-list.html',
   styleUrl: './member-list.css'
 })
 export class MemberList {
+  private MembersService = inject(MembersService);
+  protected members$: Observable<Member[]>;
 
+  constructor() {
+    this.members$ = this.MembersService.getMembers();
+  }
 }
